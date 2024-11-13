e03d286083d0810aa85689cfe2f9376a94fa35bd
kundusubrata <kundu.subrata2020@outlook.com>
2024-11-13 13:24:01 +0530

    bug fix on auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,8 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import { client } from "./sanity/lib/client";
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
-import { writeClient } from "./sanity/lib/write-client";
-
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
@@ -32,7 +31,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       return true;
     },
-
     async jwt({ token, account, profile }) {
       if (account && profile) {
         const user = await client
